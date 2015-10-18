3410a874ad4efbdf1934fc8fb398034fc7d62806
Patryk <urbanek32@o2.pl>
2015-10-18 16:42:50 +0200

    Working Login Form

--- app/src/main/java/tk/daruhq/uberoczkoprojekt/LoginActivity.java
+++ app/src/main/java/tk/daruhq/uberoczkoprojekt/LoginActivity.java
@@ -31,10 +31,12 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
@@ -48,32 +50,28 @@ import java.io.IOException;
 
 public class LoginActivity extends Activity {
 
-
-
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    //private UserLoginTask mAuthTask = null;
-
-
-    private Context loginContext;
+    private Context loginContext = this;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+    private AutoCompleteTextView nicknameView;
+    private EditText passwordView;
+    private View progressView;
+    private View loginFormView;
 
     private final OkHttpClient httpClient = new OkHttpClient();
+    private HttpAsyncLoginTask userLoginTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginContext = this;
+        nicknameView = (AutoCompleteTextView) findViewById(R.id.nickname);
+        passwordView = (EditText) findViewById(R.id.password);
+        progressView = findViewById(R.id.login_progress);
+        loginFormView = findViewById(R.id.login_form);
 
-        Button myPostButton = (Button)findViewById(R.id.postButton);
+        /*Button myPostButton = (Button)findViewById(R.id.postButton);
         myPostButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,13 +86,82 @@ public class LoginActivity extends Activity {
             public void onClick(View v) {
                 new HttpAsyncGetTask(loginContext).execute("http://eti.endrius.tk/test/api/test_empty_body");
             }
+        });*/
+
+        Button myLoginButton = (Button)findViewById(R.id.login_button);
+        myLoginButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptLogin();
+            }
         });
 
 
     }
 
-    private String GET(String... params)
-    {
+    private void attemptLogin() {
+
+        if(userLoginTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        nicknameView.setError(null);
+        passwordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String nickname = nicknameView.getText().toString();
+        String password = passwordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+
+        // Check for a valid password
+        if (TextUtils.isEmpty(password)) {
+            passwordView.setError(getString(R.string.error_field_required));
+            focusView = passwordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)) {
+            passwordView.setError(getString(R.string.error_invalid_password));
+            focusView = passwordView;
+            cancel = true;
+        }
+
+        // Check for a valid nickname
+        if (TextUtils.isEmpty(nickname)) {
+            nicknameView.setError(getString(R.string.error_field_required));
+            focusView = nicknameView;
+            cancel = true;
+        } else if (!isNicknameValid(nickname)) {
+            nicknameView.setError(getString(R.string.error_invalid_nickname));
+            focusView = nicknameView;
+            cancel = true;
+        }
+
+        if(cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            showProgress(false);
+            focusView.requestFocus();
+            return;
+        }
+
+        showProgress(true);
+        userLoginTask = new HttpAsyncLoginTask(loginContext);
+        userLoginTask.execute(
+                "http://java21.endrius.tk/login",
+                nickname,
+                password);
+
+    }
+
+    private void showProgress(boolean show) {
+        progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+    }
+
+    private String GET(String... params) {
         try {
             Request request = new Request.Builder()
                     .url(params[0])
@@ -112,14 +179,40 @@ public class LoginActivity extends Activity {
         }
     }
 
-    private String POST(String... params)
-    {
+    private String POST(String... params) {
         try {
             FormEncodingBuilder formEncodingBuilder = new FormEncodingBuilder();
             for(int i = 1; i < params.length-1; i++) {
                 formEncodingBuilder.add(params[i], params[i+1]);
             }
             RequestBody formBody = formEncodingBuilder.build();
+
+            Request request = new Request.Builder()
+                    .url(params[0])
+                    .post(formBody)
+                    .build();
+
+            Response response = httpClient.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            return response.body().string();
+        }
+        catch(IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    private String CreateLoginJSON(String nickname, String password) {
+        return String.format("{ \"nickname\": \"%s\", \"password\": \"%s\" }", nickname, password);
+    }
+
+    private String LoginPOST(String... params)
+    {
+        try {
+            MediaType JSON = MediaType.parse("application/JSON; charset=utf-8");
+            RequestBody formBody = RequestBody.create(JSON, CreateLoginJSON(params[1], params[2]));
 
             Request request = new Request.Builder()
                     .url(params[0])
@@ -146,17 +239,51 @@ public class LoginActivity extends Activity {
 
     }
 
-
-    private boolean isEmailValid(String email) {
+    private boolean isNicknameValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.length() > 3;
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.length() > 3;
     }
 
+
+
+
+
+    public class HttpAsyncLoginTask  extends AsyncTask<String, Void, String> {
+        private Context context;
+
+        public HttpAsyncLoginTask(Context context){
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // write show progress Dialog code here
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            return LoginPOST(params);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                Toast.makeText(getBaseContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                showProgress(false);
+                userLoginTask = null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
 
     public class HttpAsyncPostTask  extends AsyncTask<String, Void, String> {
@@ -179,11 +306,9 @@ public class LoginActivity extends Activity {
 
         @Override
         protected void onPostExecute(String result) {
-            TextView view = (TextView) findViewById(R.id.textView);
             try {
                 JSONObject jsonObject = new JSONObject(result);
                 Toast.makeText(getBaseContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
-                view.setText(result);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -211,11 +336,11 @@ public class LoginActivity extends Activity {
 
         @Override
         protected void onPostExecute(String result) {
-            TextView view = (TextView) findViewById(R.id.textView);
+            //TextView view = (TextView) findViewById(R.id.textView);
             try {
                 JSONObject jsonObject = new JSONObject(result);
                 Toast.makeText(getBaseContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
-                view.setText(result);
+                //view.setText(result);
 
                 Intent intent = new Intent(context, GameActivity.class);
                 startActivityForResult(intent, 1);
